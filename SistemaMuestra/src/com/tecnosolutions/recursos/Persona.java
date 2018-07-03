af1c218886ee4a8e53aaf202d0c3e1867dd3031d
UTC1992 <omar.guanoluisa25@gmail.com>
2018-07-03 12:26:33 -0500

    commit2 creacion de la estructura de paquetes y capas

---
+++ SistemaMuestra/src/com/tecnosolutions/recursos/Persona.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tecnosolutions.recursos;
+
+/**
+ *
+ * @author omar
+ */
+public class Persona {
+    int id;
+    String nombre;
+    String apellido;
+    String cedula;
+    String telefono;
+    String email;
+    String direccion;
+    
+    public Persona() {
+        this.id = 0;
+        this.nombre = "";
+        this.apellido = "";
+        this.cedula = "";
+        this.telefono = "";
+        this.email = "";
+        this.direccion = "";
+    }
+    
+    public Persona(int id, String nombre, String apellido, String cedula, String telefono, String email, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+ 
+    
+}
